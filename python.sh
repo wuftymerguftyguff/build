35a8030c0acae2ef25b422c9c0df524ff358428c
Jeff Arthur <wuftymerguftyguff@gmail.com>
2017-08-04 20:25:11 +0100

    more

--- python.sh
+++ python.sh
@@ -2,6 +2,10 @@ git clone https://github.com/python/cpython.git
 cd cpython
 git checkout 2.7
 sudo apt-get install libdb-dev
-./configure --with-pydebug --with-dbmliborder=bdb:gdbm --enable-shared --enable-ipv6
+mkdir built
+PRE=$(pwd)/built
+./configure --prefix=$PRE --with-pydebug --with-dbmliborder=bdb:gdbm --enable-shared --enable-ipv6
 make -s -j2
-
+make install
+cd built
+ls -lrt
