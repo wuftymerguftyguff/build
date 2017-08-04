61403dfdf0b5417f042bddd6648719ed009c3880
Jeff Arthur <wuftymerguftyguff@gmail.com>
2017-08-04 20:14:24 +0100

    bdb build deps

--- python.sh
+++ python.sh
@@ -1,7 +1,8 @@
 git clone https://github.com/python/cpython.git
 cd cpython
 git checkout 2.7
-sudo apt-get install db
+sudo apt-get install libdb
+sudo apt-get install libdb-dev
 ./configure --with-pydebug --with-dbmliborder=bdb:gdbm --enable-shared --enable-ipv6
 make -s -j2
 
