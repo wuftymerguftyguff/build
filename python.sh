9566d391539259b422a99440f3c4ac8a5126a3e1
Jeff Arthur <wuftymerguftyguff@gmail.com>
2017-08-04 21:32:19 +0100

    fix paths

--- python.sh
+++ python.sh
@@ -10,8 +10,8 @@ make install
 cd built
 ls -lrt
 cd bin
-export PATH=$PRE/built/bin:$PATH
-export LD_LIBRARY_PATH=$PRE/built/lib:$LD_LIBRARY_PATH
+export PATH=$PRE/bin:$PATH
+export LD_LIBRARY_PATH=$PRE/lib:$LD_LIBRARY_PATH
 which python
 python -V
 python -c "import dbm"
